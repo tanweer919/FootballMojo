50091662c548f87f0b8a79b970ed5788706e4299
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-07-05 01:19:36 +0530

    Added lazy loading logic to favourite team score tab

--- lib/widgets/AllScores.dart
+++ lib/widgets/AllScores.dart
@@ -13,10 +13,8 @@ class AllScores extends StatefulWidget {
 
 class _AllScoresState extends State<AllScores> {
   ScrollController _scrollController = ScrollController();
-  final GlobalKey<AnimatedListState> _allScoresListlKey = GlobalKey();
   int _lastRetrievedLindex = 9;
   int _totalNoOfScores;
-  int _listInitalCount = 11;
   List<Score> _scores;
   @override
   void initState() {
@@ -33,8 +31,6 @@ class _AllScoresState extends State<AllScores> {
                   _scrollController.position.pixels ==
               0.0) &&
           _lastRetrievedLindex < _totalNoOfScores - 1) {
-        print('${_scrollController.position.maxScrollExtent -
-            _scrollController.position.pixels}');
         _getMoreScores(_leagueWiseScores);
       }
     });
@@ -61,27 +57,31 @@ class _AllScoresState extends State<AllScores> {
       child: Container(
         margin: EdgeInsets.only(top: 30.0),
         child: _scores != null
-            ? AnimatedList(
-                key: _allScoresListlKey,
+            ? ListView.builder(
                 shrinkWrap: true,
-                initialItemCount: _listInitalCount,
+                itemCount: _lastRetrievedLindex + 2,
                 physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (BuildContext context, int index,
-                    Animation<double> animation) {
-                  if (index < _listInitalCount - 1) {
-                    final Score score = _scores[index];
-                    return SlideTransition(
-                      position: Tween<Offset>(
-                          begin: const Offset(-1, 0), end: Offset.zero)
-                          .animate(animation),
-                      child: ScoreCard(
-                        score: score,
-                      ),
-                    );
+                itemBuilder: (BuildContext context, int index) {
+                  if(index == _lastRetrievedLindex + 1) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: SizedBox(
+                          height: 40,
+                          child: Chip(
+                            elevation: 2,
+                            backgroundColor: Colors.white,
+                            avatar: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Icon(Icons.arrow_upward),
+                            ),
+                            label: Text('Swipe up to load more', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),),
+                          ),
+                        ),
+                      );
                   }
-                  return SizedBox(
-                    height: 80,
-                    child: SpinKitFadingCube(color: Theme.of(context).primaryColor, size: 50,),
+                  final Score score = _scores[index];
+                  return ScoreCard(
+                    score: score,
                   );
                 })
             : ListView.builder(
@@ -100,18 +100,10 @@ class _AllScoresState extends State<AllScores> {
   void _getMoreScores(List<Score> scores) {
     setState(() {
       if (_lastRetrievedLindex < (_totalNoOfScores - 11)) {
-        for (int i = 1; i <= 10; i++) {
-          _allScoresListlKey.currentState.insertItem(_lastRetrievedLindex + i);
-          _listInitalCount+=1;
-        }
         _scores.addAll(
             scores.sublist(_lastRetrievedLindex + 1 , _lastRetrievedLindex + 11));
         _lastRetrievedLindex += 10;
       } else {
-        for (int i = 1; i < (_totalNoOfScores - _lastRetrievedLindex); i++) {
-          _allScoresListlKey.currentState.insertItem(_lastRetrievedLindex + i);
-          _listInitalCount+=1;
-        }
         _scores.addAll(scores.sublist(_lastRetrievedLindex + 1));
         _lastRetrievedLindex = _totalNoOfScores - 1;
       }
--- lib/widgets/FavouriteScores.dart
+++ lib/widgets/FavouriteScores.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:pk_skeleton/pk_skeleton.dart';
 import 'package:provider/provider.dart';
 import '../Provider/AppProvider.dart';
 import '../models/Score.dart';
@@ -10,34 +12,101 @@ class FavouriteScores extends StatefulWidget {
 }
 
 class _FavouriteScoresState extends State<FavouriteScores> {
+  ScrollController _scrollController = ScrollController();
+  int _lastRetrievedLindex = 9;
+  int _totalNoOfScores;
+  List<Score> _scores;
   @override
   void initState() {
     super.initState();
     final AppProvider appProvider =
-        Provider.of<AppProvider>(context, listen: false);
+    Provider.of<AppProvider>(context, listen: false);
     if (appProvider.favouriteTeamScores == null) {
       appProvider.loadFavouriteScores();
     }
+    final List<Score> _favouriteTeamScores = appProvider.favouriteTeamScores;
+    _totalNoOfScores = _favouriteTeamScores.length;
+    _scrollController.addListener(() {
+      if ((_scrollController.position.maxScrollExtent -
+          _scrollController.position.pixels ==
+          0.0) &&
+          _lastRetrievedLindex < _totalNoOfScores - 1) {
+        _getMoreScores(_favouriteTeamScores);
+      }
+    });
+    setState(() {
+      if (_totalNoOfScores > 10) {
+        _scores = _favouriteTeamScores.sublist(0, 10);
+      } else {
+        _scores = _favouriteTeamScores;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final AppProvider appProvider = Provider.of<AppProvider>(context);
     return SingleChildScrollView(
+      controller: _scrollController,
       child: Container(
         margin: EdgeInsets.only(top: 30.0),
-        child: ListView.builder(
+        child: _scores != null
+            ? ListView.builder(
             shrinkWrap: true,
-            itemCount: 10,
+            itemCount: _lastRetrievedLindex + 2,
             physics: NeverScrollableScrollPhysics(),
             itemBuilder: (BuildContext context, int index) {
-              final Score score = appProvider.favouriteTeamScores[index];
+              if(index == _lastRetrievedLindex + 1) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: SizedBox(
+                    height: 40,
+                    child: Chip(
+                      elevation: 2,
+                      backgroundColor: Colors.white,
+                      avatar: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: Icon(Icons.arrow_upward),
+                      ),
+                      label: Text('Swipe up to load more', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),),
+                    ),
+                  ),
+                );
+              }
+              final Score score = _scores[index];
               return ScoreCard(
                 score: score,
               );
-              ;
+            })
+            : ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return PKCardSkeleton(
+                isCircularImage: true,
+                isBottomLinesActive: true,
+              );
             }),
       ),
     );
+  }
+
+  void _getMoreScores(List<Score> scores) {
+    setState(() {
+      if (_lastRetrievedLindex < (_totalNoOfScores - 11)) {
+        _scores.addAll(
+            scores.sublist(_lastRetrievedLindex + 1 , _lastRetrievedLindex + 11));
+        _lastRetrievedLindex += 10;
+      } else {
+        _scores.addAll(scores.sublist(_lastRetrievedLindex + 1));
+        _lastRetrievedLindex = _totalNoOfScores - 1;
+      }
+    });
   }
 }
