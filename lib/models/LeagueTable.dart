6da83ce20601e1eea1cb976b5ba11bb004819b29
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-07-18 01:07:30 +0530

    Added leaguetable model

---
+++ lib/models/LeagueTable.dart
@@ -0,0 +1,26 @@
+class LeagueTable {
+  int position;
+  String teamName;
+  String teamLogo;
+  int matchesPlayed;
+  int wins;
+  int draws;
+  int loses;
+  int points;
+  int goalsFor;
+  int goalsAgainst;
+  String form;
+  LeagueTable(this.position, this.teamName, this.teamLogo, this.matchesPlayed, this.wins, this.draws, this.loses, this.points, this.goalsFor, this.goalsAgainst, this.form);
+  LeagueTable.fromJson(Map<String, dynamic> unparsedJson)
+    : position = unparsedJson["rank"],
+      teamName = unparsedJson["team"]["name"],
+      teamLogo = unparsedJson["team"]["logo"],
+      matchesPlayed = unparsedJson["all"]["played"],
+      wins = unparsedJson["all"]["win"],
+      draws = unparsedJson["all"]["draw"],
+      loses = unparsedJson["all"]["lose"],
+      points = unparsedJson["points"],
+      goalsFor = unparsedJson["all"]["goals"]["for"],
+      goalsAgainst = unparsedJson["all"]["goals"]["against"],
+      form = unparsedJson["form"];
+}
